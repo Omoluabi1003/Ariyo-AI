5d5d37e121a16882893e60b2611ee4ae791a0e5b
Omoluabi1003 <pakiyogun10@gmail.com>
2025-07-21 02:36:41 -0400

    refactor: use shared logo constant

--- scripts/data.js
+++ scripts/data.js
@@ -97,13 +97,30 @@ const albums = [
     }
 ];
 
+const sharedLogo = "https://raw.githubusercontent.com/Omoluabi1003/Ariyo-AI/main/logo.jpg";
+
 const radioStations = [
-  { name: "Cool FM", location: "Lagos", url: "https://coolfm-edge-02-gos.sharp-stream.com/coolfm_lagos_aac", logo: "Nigeria Pix.png" },
-  { name: "Wazobia FM", location: "Lagos", url: "https://wazobiafm-edge-01-gos.sharp-stream.com/wazobia_lagos_aac", logo: "Nigeria Pix.png" },
-  { name: "Soundcity Radio", location: "Lagos", url: "https://soundcity.stream.refreshed.co/sc985", logo: "Nigeria Pix.png" },
-  { name: "Splash FM", location: "Ibadan", url: "https://radio-edge-01.streamafrica.net/splashibadan", logo: "Nigeria Pix.png" },
-  { name: "Freedom Radio", location: "Kano", url: "https://freedomradionig.com/live-radio/", logo: "Nigeria Pix.png" },
-  { name: "Arewa Radio", location: "Kano", url: "https://arewaradio.com/live-radio/", logo: "Nigeria Pix.png" },
-  { name: "Joy FM", location: "Accra", url: "https://stream.zeno.fm/t3q5h7z1qtruv", logo: "Lagos traffic.png" },
-  { name: "BBC News", location: "International", url: "https://stream.live.vc.bbcmedia.co.uk/bbc_world_service", logo: "Abuja City Gate.png" }
+  { name: "Cool FM", location: "Lagos", url: "https://coolfm-edge-02-gos.sharp-stream.com/coolfm_lagos_aac", logo: sharedLogo },
+  { name: "Wazobia FM", location: "Lagos", url: "https://wazobiafm-edge-01-gos.sharp-stream.com/wazobia_lagos_aac", logo: sharedLogo },
+  { name: "Soundcity Radio", location: "Lagos", url: "https://soundcity.stream.refreshed.co/sc985", logo: sharedLogo },
+  { name: "Splash FM", location: "Ibadan", url: "https://radio-edge-01.streamafrica.net/splashibadan", logo: sharedLogo },
+  { name: "Freedom Radio", location: "Kano", url: "https://freedomradionig.com/live-radio/", logo: sharedLogo },
+  { name: "Arewa Radio", location: "Kano", url: "https://arewaradio.com/live-radio/", logo: sharedLogo },
+  { name: "Joy FM", location: "Accra", url: "https://stream.zeno.fm/t3q5h7z1qtruv", logo: sharedLogo },
+  { name: "BBC News", location: "International", url: "https://stream.live.vc.bbcmedia.co.uk/bbc_world_service", logo: sharedLogo },
+  { name: "702 Johannesburg", location: "Johannesburg, South Africa", url: "http://25693.live.streamtheworld.com:3690/FM702_SC", logo: sharedLogo },
+  { name: "94.7", location: "Johannesburg, South Africa", url: "http://25343.live.streamtheworld.com:3690/FM947AAC_SC", logo: sharedLogo },
+  { name: "Energy 100 FM", location: "Windhoek, Namibia", url: "https://s2.stationplaylist.com:7094/listen.aac", logo: sharedLogo },
+  { name: "Fresh FM", location: "Windhoek, Namibia", url: "https://edge.iono.fm/xice/192_medium.aac", logo: sharedLogo },
+  { name: "Hot FM 87.7", location: "Lusaka, Zambia", url: "http://s2.yesstreaming.net:7091/stream?type=.mp3%2F%3Bstream.mp3&_=1", logo: sharedLogo },
+  { name: "Sun FM Zambia", location: "Zambia", url: "http://11233.cloudrad.io:9102/live", logo: sharedLogo },
+  { name: "Gabz FM 96.2", location: "Gaborone, Botswana", url: "http://fmt01.egihosting.com:17721/;", logo: sharedLogo },
+  { name: "Hope Radio 102.6 FM", location: "Kabale, Uganda", url: "http://stream-175.zeno.fm/t4598wzxfkhvv", logo: sharedLogo },
+  { name: "DFM Radio Rwanda", location: "Rwanda", url: "https://stream-176.zeno.fm/fygodpmrgcquv", logo: sharedLogo },
+  { name: "Capital Radio", location: "Tanzania", url: "https://capitalradio.radioca.st/stream", logo: sharedLogo },
+  { name: "Capitalk 100.4FM", location: "Harare, Zimbabwe", url: "https://edge.iono.fm/xice/162_medium.aac", logo: sharedLogo },
+  { name: "Star FM 89.7", location: "Zimbabwe", url: "https://edge.iono.fm/xice/159_medium.aac", logo: sharedLogo },
+  { name: "ADUM RADIO", location: "Ghana", url: "http://stream-176.zeno.fm/dmn9atfb7ehvv", logo: sharedLogo },
+  { name: "Voice of Ethiopia", location: "Ethiopia", url: "https://stream-173.zeno.fm/hubm64kkr2zuv", logo: sharedLogo },
+  { name: "919", location: "South Africa", url: "https://edge.iono.fm/xice/112_medium.aac", logo: sharedLogo }
 ];
