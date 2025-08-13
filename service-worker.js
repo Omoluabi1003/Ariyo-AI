8d152aa0f217e9d7719b9bc0d9cf368898065d30
Omoluabi1003 <pakiyogun10@gmail.com>
2025-08-12 21:00:01 -0400

    style: move tetris controls beside playfield

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,9 @@ self.addEventListener('install', event => {
           'picture-game.html',
           'picture-game.css',
           'picture-game.js',
+          'tetris.html',
+          'tetris.css',
+          'tetris.js',
           'offline-audio.mp3'
         ];
         return caches.open(CACHE_NAME).then(cache => {
