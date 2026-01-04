c7e73d5b01a35a5eec81a923d09d42b03f36fcae
Omoluabi1003 <pakiyogun10@gmail.com>
2026-01-04 16:35:09 -0500

    Remove Crew Console feature

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-const { clearCookie, SESSION_COOKIE } = require('./utils');
-
-module.exports = async (req, res) => {
-  res.setHeader('Set-Cookie', clearCookie(SESSION_COOKIE));
-  res.writeHead(302, { Location: '/crew/' });
-  res.end();
-};
