b77abbdac7aa4d20ea5ab1b6e6c898030f15a8c8
Omoluabi1003 <pakiyogun10@gmail.com>
2026-04-17 09:25:32 -0400

    Remove 'I Love You' from Omoluabi catalogue

--- scripts/data.js
+++ scripts/data.js
@@ -238,7 +238,6 @@ const albums = [
               { src: 'https://cdn1.suno.ai/8d4e57ee-c330-495e-a9d0-75a861a4420f.mp3', title: 'Udo Don Cost' },
               { src: 'https://cdn1.suno.ai/377a7311-7338-4a51-816a-96eacaef7bac.mp3', title: 'Matasa Ku Tashi' },
               { src: 'https://cdn1.suno.ai/1dcdb3cf-5397-41d0-b005-f79055bf5a56.mp3', title: 'Feelings Fi You ft. Steady' },
-              { src: 'https://cdn1.suno.ai/ab702e5a-f698-4e05-9463-499cbeccff67.mp3', title: 'I Love You' },
               { src: 'https://cdn1.suno.ai/4a173806-00d5-4528-be27-3f236df02c58.mp3', title: 'Face Of A Narcissist' },
               { src: 'https://cdn1.suno.ai/52e7e84c-205b-4439-9c59-7e21304ca168.mp3', title: 'Growth Comes With Goodbyes' },
             { src: 'https://cdn1.suno.ai/e75f0166-5134-4538-b33d-4bbdf708e53e.mp3', title: 'Boda Yen' },
@@ -612,13 +611,6 @@ const latestTrackAnnouncements = [
       addedOn: '2025-02-24T09:00:00Z',
       isFreshDrop: true
     },
-    {
-      albumName: 'Omoluabi Production Catalogue',
-      title: 'I Love You',
-    src: 'https://cdn1.suno.ai/ab702e5a-f698-4e05-9463-499cbeccff67.mp3',
-    addedOn: '2025-12-04T09:00:00Z',
-    isFreshDrop: true
-  },
   {
     albumName: 'Omoluabi Production Catalogue',
     title: 'Face Of A Narcissist',
